2c50688b62fdeccb5598ddba0c5e9d93f3a76d56
momensale7 <momensale2468@gmail.com>
2025-03-26 17:12:21 +0200

    setup routing

--- src/modules/shared/AuthLayout/AuthLayout.tsx
+++ src/modules/shared/AuthLayout/AuthLayout.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 export default function AuthLayout() {
   return (
-    <div>AuthLayout</div>
+    <div><Outlet/></div>
   )
 }
