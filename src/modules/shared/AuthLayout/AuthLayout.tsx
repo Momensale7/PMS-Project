c5f2f8bb77e91a4dfe0bcf85233a115d49221338
Mohammed Mobarak <mohammeda.mobarak@gmail.com>
2025-03-27 12:53:51 +0200

    MM-authlayout-v2 (logo added)

--- src/modules/shared/AuthLayout/AuthLayout.tsx
+++ src/modules/shared/AuthLayout/AuthLayout.tsx
@@ -1,17 +1,24 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
+import logo from '../../../assets/images/PMSlogo.png'
 
 export default function AuthLayout() {
-
   return (
-<div className="auth-container">
-        <div className="vector-rightImg">
-        </div>
+    <Container fluid className="auth-container">
+      {/* backgrounds */}
+      <div className="vector-rightImg"></div>
+      <div className="vector-leftImg"></div>
 
-        <div className="vector-leftImg">
-        </div>
-    <div><Outlet/></div>
-    </div>
+      {/* outlet at center*/}
+      <Row className="justify-content-center align-items-center vh-100">
+        <Col xs={12} md={8} lg={6} className="auth-content text-center">
 
-  )
+        <img src={logo}/>
+
+          <Outlet />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
