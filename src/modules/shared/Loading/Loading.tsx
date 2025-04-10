61cbd05908fbb05eeb7840d44d516b0831c948ea
ahm6dalii <ahmedalielian20@gmail.com>
2025-04-11 01:03:55 +0200

    make cus hook for pagination method and state and add pagintion in project and chage pagintion color in dark mood

--- src/modules/shared/Loading/Loading.tsx
+++ src/modules/shared/Loading/Loading.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Loading() {
   return (
-    <div>Loading</div>
+   <div className='text-center  d-flex justify-content-center align-items-center w-100'>
+        <i className='fa fa-spinner fa-spin text-success  bg-transparent' ></i>
+      </div>
   )
 }
--- src/modules/shared/Pagination/Pagination.tsx
+++ src/modules/shared/Pagination/Pagination.tsx
@@ -16,7 +16,7 @@ const Pagination: React.FC<PaginationProps> = ({
   const totalPages = Math.ceil(totalItems / pageSize);
 
   return (
-    <div className="d-flex justify-content-end align-items-center px-4 py-3">
+    <div className="dark-text d-flex justify-content-end align-items-center px-4 py-3">
       {/* Left - Show per page */}
       <div
         className="d-flex align-items-center gap-2"
@@ -43,14 +43,14 @@ const Pagination: React.FC<PaginationProps> = ({
       </div>
 
       {/* Center - Arrows + page info */}
-      <div className="d-flex align-items-center gap-3">
+      <div className="d-flex align-items-center gap-3 dark-text">
         <span className="d-flex align-items-center gap-2">
-          <div style={{ color: "gray", marginRight: "1rem" }}>
+          <div className="dark-pagnaition-color" style={{ marginRight: "1rem" }}>
             {" "}
             of <strong>{totalItems }</strong> {label}
           </div>
 
-          <span style={{ color: "gray" }}>
+          <span className="dark-pagnaition-color" >
             Page <strong>{pageNumber}</strong> of <strong>{totalPages}</strong>
           </span>
 
@@ -58,19 +58,19 @@ const Pagination: React.FC<PaginationProps> = ({
         </span>
 
         <button
-          className="btn btn-outline-secondary btn-sm"
+          className="btn btn-outline-secondary border-color  btn-sm dark-text"
           onClick={handlePrev}
           disabled={pageNumber === 1}
         >
-          <FaArrowLeft />
+          <FaArrowLeft className="dark-text" />
         </button>
 
         <button
-          className="btn btn-outline-secondary btn-sm"
+          className="btn btn-outline-secondary border-color btn-sm dark-text"
           onClick={handleNext}
           disabled={pageNumber === totalPages}
         >
-          <FaArrowRight />
+          <FaArrowRight className="dark-text" />
         </button>
       </div>
     </div>
