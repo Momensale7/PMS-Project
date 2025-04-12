08fc415dce4cc01274f5907d1712bcdf61c628bb
momensale7 <momensale2468@gmail.com>
2025-04-12 17:33:50 +0200

    fix build errors

--- src/modules/AuthContext/AuthContext.tsx
+++ src/modules/AuthContext/AuthContext.tsx
@@ -1,7 +1,10 @@
 import  { createContext,  useEffect, useState } from 'react'
 import { jwtDecode } from 'jwt-decode';
-import { AuthContextProviderProps, AuthContextType, UserTokenPayload } from '../Interfaces/AuthInterfaces';
-
+import {
+    AuthContextProviderProps,
+    AuthContextType,
+    UserTokenPayload,
+  } from '../Interfaces/AuthInterfaces';
 
 export const Authcontext = createContext<AuthContextType | null>(null);
 export default function AuthContextProvider({ children }: AuthContextProviderProps) {
